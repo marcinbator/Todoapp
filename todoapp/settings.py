7bba7cf679f8b4333993d817ffb7eb5c9a7675fb
Marcinek <marcinbator.ofc@gmail.com>
2023-02-23 20:20:14 +0100

    main page style

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -113,14 +113,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = 'users/login'
+LOGIN_URL = '/users/login'
 
 X_FRAME_OPTIONS = '*'
 
